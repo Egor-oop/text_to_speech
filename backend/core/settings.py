aefe4a0175ba765ae3e9d5e3fee7e10c9a39b63a
Egor-oop <gulidoegor@gmail.com>
2023-01-27 17:16:36 +0300

    feat: docker

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv('DEBUG') == True
 
 ALLOWED_HOSTS = ['*']
 
