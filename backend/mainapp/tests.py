f562d08ce9f73ea11a173a703087a1d814614154
Egor-oop <gulidoegor@gmail.com>
2023-02-06 18:35:46 +0300

    feat[tests]: created test for convert

--- backend/mainapp/tests.py
+++ backend/mainapp/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import RequestsClient, APITestCase, CoreAPIClient, \
+    APIRequestFactory, APIClient
+from rest_framework import status
 
-# Create your tests here.
+client = APIClient()
+
+
+class ConvertTTSTest(APITestCase):
+    def setUp(self) -> None:
+        self.payload = {
+            "text": {
+                "text": "First Day as a Software Engineer",
+                "lang": "en"
+            }
+        }
+
+    def test_convert_valid_payload_with_english(self):
+        response = client.post(
+            reverse('convert'),
+            self.payload,
+            format='json',
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
