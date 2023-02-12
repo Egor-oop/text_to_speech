56f527ba1d6c977238c37236c25354a22050b0e4
Egor-oop <gulidoegor@gmail.com>
2023-02-12 16:41:28 +0300

    feat[tests]: added 3 more tests

--- backend/mainapp/tests.py
+++ backend/mainapp/tests.py
@@ -1,6 +1,5 @@
 from django.urls import reverse
-from rest_framework.test import RequestsClient, APITestCase, CoreAPIClient, \
-    APIRequestFactory, APIClient
+from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
 
 client = APIClient()
@@ -8,7 +7,7 @@ client = APIClient()
 
 class ConvertTTSTest(APITestCase):
     def setUp(self) -> None:
-        self.payload = {
+        self.valid_payload_en = {
             "text": {
                 "text": "First Day as a Software Engineer",
                 "lang": "en",
@@ -16,17 +15,56 @@ class ConvertTTSTest(APITestCase):
             }
         }
 
-    # {
-    #     "text": {
-    #         "text": "Привет, человек!",
-    #         "lang": "ru",
-    #         "speed": 1.2
-    #     }
-    # }
-    def test_convert_valid_payload_with_english(self):
+        self.invalid_payload_en = {
+            "text": {
+                "text": "First Day as a Software Engineer",
+                "lang": "en"
+            }
+        }
+
+        self.valid_payload_ru = {
+            "text": {
+                "text": "Если ты нормальный нажми 1234",
+                "lang": "ru",
+                "speed": 1.5
+            }
+        }
+
+        self.invalid_payload_ru = {
+            "text": {
+                "text": "Если ты нормальный нажми 1234",
+                "lang": "ru"
+            }
+        }
+
+    def test_convert_valid_payload_en(self):
         response = client.post(
             reverse('convert'),
-            self.payload,
+            self.valid_payload_en,
             format='json',
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_convert_invalid_payload_en(self):
+        response = client.post(
+            reverse('convert'),
+            self.invalid_payload_en,
+            format='json',
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_convert_valid_payload_ru(self):
+        response = client.post(
+            reverse('convert'),
+            self.valid_payload_ru,
+            format='json',
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_convert_invalid_payload_ru(self):
+        response = client.post(
+            reverse('convert'),
+            self.invalid_payload_en,
+            format='json',
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
