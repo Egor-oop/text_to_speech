710fb31ff768b93058f0c001fcd42b2e703affad
Egor-oop <gulidoegor@gmail.com>
2023-02-12 17:02:47 +0300

    chores[refactor]: separate speed changing in another method

--- backend/mainapp/convert.py
+++ backend/mainapp/convert.py
@@ -1,11 +1,13 @@
 from gtts import gTTS
 from datetime import datetime
-import soundfile
-import pyrubberband
-import os
-
 from pydub import AudioSegment
 from pydub.effects import speedup
+
+
+def change_speed(file: str, speed: float) -> None:
+    audio = AudioSegment.from_file(file)
+    audio = speedup(audio, playback_speed=speed, chunk_size=100, crossfade=200)
+    audio.export(file, format='wav')
 
 
 def convert(text: str, lang: str, speed: float) -> str:
@@ -13,11 +15,5 @@ def convert(text: str, lang: str, speed: float) -> str:
     path = f'media/audio/{datetime.now()}.wav'
     file.save(path)
     if speed != 1.0:
-        # y, sr = soundfile.read(path)
-        # y_stretch = pyrubberband.time_stretch(y, sr, speed)
-        # soundfile.write(path, y_stretch, sr, format='wav')
-        audio = AudioSegment.from_file(path)
-        audio = speedup(audio, playback_speed=speed, chunk_size=100, crossfade=200)
-        audio.export(path, format='wav')
+        change_speed(path, speed)
     return path
-# { "text": { "text": "Если ты нормальный нажми 1234", "lang": "ru", "speed": 1.5 } }
